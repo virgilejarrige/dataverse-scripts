d99c89b23eaa804dc6eb479b3b54b68995d3c9ec
Virgile Jarrige <67372423+virgilejarrige@users.noreply.github.com>
2023-07-10 05:27:44 +0200

    Update 2-link-datasets-with-specific-affiliation.py
    Uses user_variables.txt

--- 2-link-datasets-with-specific-affiliation.py
+++ 2-link-datasets-with-specific-affiliation.py
@@ -1,21 +1,13 @@
 import csv
 import requests
+import json
 
-# Modify these variables
-# Set the base URL of your Dataverse installation
-base_url = "https://dataverse_url.com/api"
+# Read user variables from the text file
+with open("user_variables.txt", mode="r") as var_file:
+    user_variables = var_file.read()
 
-# Set the API token for authentication
-api_token = "your_secret_token"
-
-# Set the CSV file path
-csv_filename = "dataset_title_affiliation_collectionid.csv"  # Update with the appropriate CSV filename
-
-# Set the target collection ID
-collection_id = "target_collection"  # Replace with the ID of the desired collection
-
-# Set the affiliation pattern to be matched
-affiliation_pattern = "pattern"  # Update with the desired affiliation pattern
+# Evaluate the user variables as Python code
+exec(user_variables)
 
 #---------------------------
 # Don't modify any further
@@ -25,38 +17,41 @@ headers = {
     "Content-type": "application/json"
 }
 
-# Read the CSV file and link datasets with the defined affiliation pattern to the collection
+# Read the CSV file and link datasets with the defined affiliation pattern to the target collection
 with open(csv_filename, mode="r", encoding="utf-8") as file:
     reader = csv.reader(file)
     header = next(reader)  # Skip the header row
     datasets = list(reader)
 
-    # Iterate over each dataset and link it to the collection based on the defined affiliation pattern
+    # Iterate over each dataset and link it to the target collection based on the defined affiliation pattern
     for dataset in datasets:
         title, affiliation, global_id = dataset
 
         # Check if the affiliation pattern is found in the dataset's affiliation
         if affiliation_pattern in affiliation:
             # Get the dataset ID from the persistent identifier
-            dataset_endpoint = f"{base_url}/datasets/:persistentId/"
+            dataset_endpoint = f"{base_url}/api/datasets/:persistentId/"
             params = {
                 "persistentId": global_id
             }
-            response = requests.get(dataset_endpoint, params=params, headers=headers)
-            data = response.json()
-            dataset_id = data["data"]["id"]
+            try:
+                response = requests.get(dataset_endpoint, params=params, headers=headers)
+                data = response.json()
+                dataset_id = data["data"]["id"]
 
-            # Create the link API endpoint
-            link_endpoint = f"{base_url}/datasets/{dataset_id}/link/{collection_id}"
+                # Create the link API endpoint
+                link_endpoint = f"{base_url}/api/datasets/{dataset_id}/link/{target_collection}"
 
-            # Send a PUT request to link the dataset to the collection
-            response = requests.put(link_endpoint, headers=headers)
+                # Send a PUT request to link the dataset to the target collection
+                response = requests.put(link_endpoint, headers=headers)
 
-            # Check if the request was successful (HTTP status code 200)
-            if response.status_code == 200:
-                print(f"Dataset '{title}' linked to collection with ID '{collection_id}'")
-            else:
-                print(f"Failed to link dataset '{title}' to collection with ID '{collection_id}': {response.text}")
+                # Check if the request was successful (HTTP status code 200)
+                if response.status_code == 200:
+                    print(f"Dataset '{title}' linked to collection with ID '{target_collection}'")
+                else:
+                    print(f"Failed to link dataset '{title}' to collection with ID '{target_collection}': {response.text}")
+            except json.JSONDecodeError as e:
+                print(f"Failed to parse the JSON response for dataset '{title}': {e}")
         else:
             print(f"Skipping dataset '{title}' as it does not match the affiliation pattern")
 
