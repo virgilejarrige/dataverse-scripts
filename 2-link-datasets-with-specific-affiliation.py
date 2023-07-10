8f20c39f293b2156ad1e4f5e552356ba789faf41
Virgile Jarrige <67372423+virgilejarrige@users.noreply.github.com>
2023-07-10 05:33:40 +0200

    Update 2-link-datasets-with-specific-affiliation.py

--- 2-link-datasets-with-specific-affiliation.py
+++ 2-link-datasets-with-specific-affiliation.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Read user variables from the text file
-with open("user_variables.txt", mode="r") as var_file:
+with open("user_variables.py", mode="r") as var_file:
     user_variables = var_file.read()
 
 # Evaluate the user variables as Python code
