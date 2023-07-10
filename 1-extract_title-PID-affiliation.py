f2ff532dadbdff0fb74bc95c5a4b7381098b05b0
Virgile Jarrige <67372423+virgilejarrige@users.noreply.github.com>
2023-07-10 03:43:40 +0200

    Update 1-extract_title-PID-affiliation.py
    Fixed affiliation and title

--- 1-extract_title-PID-affiliation.py
+++ 1-extract_title-PID-affiliation.py
@@ -28,8 +28,8 @@ params = {
     "type": "dataset",
     "subtree": collection,
     "metadata_fields": "citation:*",
-    "per_page": "100",
-    "start": "0"
+    "start": 0,
+    "per_page": 100
 }
 
 # Send the initial request to retrieve the first page of results
@@ -52,9 +52,10 @@ if response.status_code == 200:
 
         # Iterate until all results are retrieved
         start = 0
-        retrieved_count = 0
+        page = 1
         while start < total_count:
-            params["start"] = str(start)
+            print(f"Retrieving page {page} - {start+1} to {min(start+params['per_page'], total_count)}")
+            params["start"] = start
             response = requests.get(search_endpoint, params=params, headers=headers)
 
             # Check if the request was successful (HTTP status code 200)
@@ -75,26 +76,22 @@ if response.status_code == 200:
                     if "citation" in metadata_blocks and "fields" in metadata_blocks["citation"]:
                         fields = metadata_blocks["citation"]["fields"]
                         for field in fields:
-                            if "datasetContactAffiliation" in field.get("value", []):
+                            if "datasetContactAffiliation" in field["value"][0]:
                                 affiliation = field["value"][0]["datasetContactAffiliation"]["value"]
                                 break
 
                     global_id = dataset["global_id"]
 
-                    # Escape the commas in the title
-                    title = title.replace(",", "\\,")
+                    # Escape the commas in the title and affiliation, enclosing them in double quotation marks
+                    title = title.replace(",", "\\,").replace('"', '""')
+                    affiliation = affiliation.replace(",", "\\,").replace('"', '""')
 
-                    writer.writerow([title, affiliation, global_id])
-
-                    retrieved_count += 1
+                    writer.writerow([f'"{title}"', f'"{affiliation}"', global_id])
 
                 start += len(datasets)
-
-                # Calculate the remaining datasets
-                remaining_count = total_count - retrieved_count
-                print(f"Retrieved: {retrieved_count} | Remaining: {remaining_count}")
+                page += 1
             else:
-                print(f"Error retrieving datasets: {response.text}")
+                print(f"Error retrieving page {page}: {response.text}")
                 break
 
     print(f"CSV file '{csv_filename}' has been created.")
