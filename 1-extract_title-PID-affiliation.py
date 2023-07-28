8b34bd80fe79f51e3442abe5abdb827389bf6e89
Virgile Jarrige <67372423+virgilejarrige@users.noreply.github.com>
2023-07-28 15:36:51 +0200

    Update 1-extract_title-PID-affiliation.py
    added author affiliation column

--- 1-extract_title-PID-affiliation.py
+++ 1-extract_title-PID-affiliation.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import re
 
 # Read user variables from the text file
 with open("user_variables.py", mode="r") as var_file:
@@ -8,8 +9,6 @@ with open("user_variables.py", mode="r") as var_file:
 # Evaluate the user variables as Python code
 exec(user_variables)
 
-#-------------------------------------
-# Don't modify any further!
 # Set the search API endpoint
 search_endpoint = f"{base_url}/api/search"
 
@@ -29,8 +28,12 @@ params = {
     "per_page": 100
 }
 
+# Remove "https://" from base_url
+base_url_clean = re.sub(r"https://", "", base_url)
+
 # Send the initial request to retrieve the first page of results
 response = requests.get(search_endpoint, params=params, headers=headers)
+
 
 # Check if the request was successful (HTTP status code 200)
 if response.status_code == 200:
@@ -42,10 +45,11 @@ if response.status_code == 200:
     print("Total results:", total_count)
 
     # Create a CSV file to store the results
-    csv_filename = f"dataset_title_affiliation_{collection}.csv"
+    csv_filename = f"{base_url_clean}_{collection}_datasets_with_affiliations.csv"
+#    csv_filename = f"dataset_title_affiliation_producer_{collection}.csv"
     with open(csv_filename, mode="w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Title", "Affiliation", "Global ID"])
+        writer.writerow(["Title", "Affiliation", "Producer", "Global ID"])
 
         # Iterate until all results are retrieved
         start = 0
@@ -63,13 +67,14 @@ if response.status_code == 200:
                 # Extract the dataset items from the response
                 datasets = data["data"]["items"]
 
-                # Iterate over each dataset and write its title, affiliation, and global ID to the CSV file
+                # Iterate over each dataset and write its title, affiliation, producer, and global ID to the CSV file
                 for dataset in datasets:
                     title = dataset["name"]
                     metadata_blocks = dataset["metadataBlocks"]
 
-                    # Initialize affiliation to None
+                    # Initialize affiliation and producer to None
                     affiliation = None
+                    producer = None
 
                     try:
                         # Extract the affiliation from the metadata blocks
@@ -78,18 +83,22 @@ if response.status_code == 200:
                             for field in fields:
                                 if "datasetContactAffiliation" in field["value"][0]:
                                     affiliation = field["value"][0]["datasetContactAffiliation"]["value"]
+                                if "producerName" in field["value"][0]:
+                                    producer = field["value"][0]["producerName"]["value"]
                                     break
                     except KeyError:
                         pass
 
                     global_id = dataset["global_id"]
 
-                    # Escape the commas in the title and affiliation, enclosing them in double quotation marks
+                    # Escape the commas in the title, affiliation, and producer, enclosing them in double quotation marks
                     title = title.replace(",", "\\,").replace('"', '""')
                     if affiliation:
                         affiliation = affiliation.replace(",", "\\,").replace('"', '""')
+                    if producer:
+                        producer = producer.replace(",", "\\,").replace('"', '""')
 
-                    writer.writerow([f'"{title}"', f'"{affiliation}"', global_id])
+                    writer.writerow([f'"{title}"', f'"{affiliation}"', f'"{producer}"', global_id])
 
                 start += len(datasets)
                 page += 1
